3ca85db13ead9f392619bd26ac7c88ae21aadcbc
ja-ho <ja-ho@localhost.localdomain>
2018-12-12 18:05:28 +0900

    add model, url_conf, view

--- books/views.py
+++ books/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from books.models import Book, Author, Publisher
 # Create your views here.
+
+#-- TemplateView
+class BooksModelView(TemplateView):
+    template_name = 'books/index.html'
+
+    def get_context_data(self, **kwagrs):
+        context = super().get_context_data(**kwagrs)
+        context['model_list'] = ['Book', 'Author', 'Publihser']
+        return context
+
+#-- ListView
+class BookList(ListView):
+    model = Book
+
+class AuthorList(ListView):
+    model = Author
+
+class PublisherList(ListView):
+    model = Publisher
+
+#-- DetailView
+class BookDetail(DetailView):
+    model = Book
+
+class AuthorDetail(DetailView):
+    model = Author
+
+class PublisherDetail(DetailView):
+    model = Publisher
+
+
