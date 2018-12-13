4e8c62063fe3b84b321cbb9cb82138f9633b2fd7
ja-ho <ja-ho@localhost.localdomain>
2018-12-13 15:01:01 +0900

    finish polls application

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from polls.models import Question, Choice
 
 # Create your views here.
