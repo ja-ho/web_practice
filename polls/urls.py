3d0f919a97635ee198e5912a263bb6e523bc58ab
ja-ho <ja-ho@localhost.localdomain>
2018-12-14 18:13:06 +0900

    change in app polls, function-based view to class-based view

--- polls/urls.py
+++ polls/urls.py
@@ -4,8 +4,11 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-        path('', views.index, name='index'),
-        path('<int:question_id>/', views.detail, name='detail'),
-        path('<int:question_id>/results/', views.results, name='results'),
+        # /polls/
+        path('', views.IndexView.as_view(), name='index'),
+        # /polls/99/
+        path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+        # /polls/99/results/
+        path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
         path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,8 +3,51 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from polls.models import Question, Choice
 
+from django.views import generic
 # Create your views here.
 
+# current code (class view)
+
+#-- Class-based Generic View
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        #"""최근 생성된 질문 5개를 반환한"""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+#-- Function-based View
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choide = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 설문 투표 폼을 다시 보여준다
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # POST 데이터를 정상적으로 처리하였다면, 항상 HttpResponseRedirect를
+        # 반환하여 리다이렉션 처리함
+        return HttpResponseRedirect(reverse('polls:results',
+            args=(question.id,)))
+
+# previous code (functional view)
+'''
 def index(request):
     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
@@ -36,5 +79,5 @@ def results(request, question_id):
     context = {'question': question}
     return render(request, 'polls/results.html', context)
 
-
+'''
 
