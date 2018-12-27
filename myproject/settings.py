7546a1bf4598099c81e1738029d8852f18f577ac
ja-ho <ja-ho@localhost.localdomain>
2018-12-27 18:39:03 +0900

    add logging

--- myproject/settings.py
+++ myproject/settings.py
@@ -125,3 +125,29 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 장고의 디폴트 설정을 유지하면서 로깅 설정함
+LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'verbose': {
+                'format' : "{%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+                'datefmt' : "%d/%b/%y %H:%M:%S"
+            },
+        },
+        'handlers': {
+            'file': {
+                'level': 'DEBUG',
+                'class': 'logging.FileHandler',
+                'filename': os.path.join(BASE_DIR, 'logs', 'myproject.log'),
+                'formatter': 'verbose'
+            },
+        },
+        'loggers': {
+            'polls': {
+                'handlers': ['file'],
+                'level': 'DEBUG',
+            },
+        },
+}
--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,9 @@ from django.urls import reverse
 from polls.models import Question, Choice
 
 from django.views import generic
+#logging 추가
+import logging
+logger = logging.getLogger(__name__)
 # Create your views here.
 
 # current code (class view)
@@ -30,6 +33,7 @@ class ResultsView(generic.DetailView):
 #-- Function-based View
 
 def vote(request, question_id):
+    logger.debug("vote().question_id: %s" % question_id)
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
